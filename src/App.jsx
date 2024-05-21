b0d883afae92a85b92ef6f40ff2edcdbd3394d29
Fazle Rabbi <35403788+irabbi360@users.noreply.github.com>
2024-05-21 10:25:45 +0600

    counter & total count add passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,47 @@
 import Counter from './components/Counter'
+import Stats from "./components/Stats.jsx";
+import {useState} from "react";
+
+const initialCounters = [
+    {
+        id: 1,
+        value: 0,
+    },
+    {
+        id: 2,
+        value: 0,
+    },
+];
 
 function App() {
+const [counters, setCounters] = useState(initialCounters);
+
+const totalCount = counters.reduce((sum, current) => sum + current.value, 0);
+
+const handleIncrement = (counterId) => {
+    const updateCounters = counters.map(counter => {
+        if (counter.id === counterId) {
+            return {
+                ...counter,
+                value: counter.value + 1
+            }
+        }
+        return counter;
+    });
+    setCounters(updateCounters);
+}
+const handleDecrement = (counterId) => {
+    const updateCounters = counters.map(counter => {
+        if (counter.id === counterId) {
+            return {
+                ...counter,
+                value: counter.value - 1
+            }
+        }
+        return counter;
+    });
+    setCounters(updateCounters);
+}
 
   return (
     <>
@@ -8,8 +49,17 @@ function App() {
         <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
           Simple Counter Application
         </h1>
-        <Counter />
-        <Counter />
+          {counters.map((counter) => (
+              <Counter
+                  count={counter.value}
+                  key={counter.id}
+                  onIncrement={() => handleIncrement(counter.id)}
+                  onDecrement={() => handleDecrement(counter.id)}
+              />
+          ))}
+
+
+          <Stats count={totalCount} />
       </div>
     </>
   )
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,25 +1,18 @@
-import { useState } from "react";
 import Count from "../components/Count";
 import Button from "../components/Button";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
+export default function Counter({ count, onIncrement, onDecrement}) {
 
-    const handleIncrement = () => {
-        setCount(prevCount => prevCount + 1);
-    };
-
-    const handleDecrement = () => {
-        setCount(prevCount => prevCount - 1);
-    };
 
     return (
-        <div className="p-5 h-auto mb-5 flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-            <Count count={count}/>
-            <div className="flex space-x-3">
-                <Button handler={handleIncrement}>Increment</Button>
-                <Button type="danger" handler={handleDecrement}>Decrement</Button>
+        <>
+            <div className="p-5 h-auto mb-5 flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+                <Count count={count}/>
+                <div className="flex space-x-3">
+                    <Button handler={onIncrement}>Increment</Button>
+                    <Button type="danger" handler={onDecrement}>Decrement</Button>
+                </div>
             </div>
-        </div>
+        </>
     );
 } 
