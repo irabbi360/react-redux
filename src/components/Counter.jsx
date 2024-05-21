075117e1f09a264449b7e7aa6f7c2b8c867c8eeb
Fazle Rabbi <35403788+irabbi360@users.noreply.github.com>
2024-05-21 09:26:52 +0600

    Initial commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Count from "../components/Count";
+import Button from "../components/Button";
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () => {
+        setCount(prevCount => prevCount + 1);
+    };
+
+    const handleDecrement = () => {
+        setCount(prevCount => prevCount - 1);
+    };
+
+    return (
+        <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+            <Count count={count}/>
+            <div className="flex space-x-3">
+                <Button handler={handleIncrement}>Increment</Button>
+                <Button type="danger" handler={handleDecrement}>Decrement</Button>
+            </div>
+        </div>
+    );
+} 
